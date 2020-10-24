607d976bce0ccce5341d3cc69afa53dec154a750
facundo cordoba <facucor.15@gmail.com>
2020-10-24 15:41:55 -0300

    P.S-01: variables and operators

---
+++ main.js
@@ -0,0 +1,19 @@
+// First exercise : Variables and Operators
+// a)
+var num1 = 5;
+var num2 = 15;
+var suma = num1 + num2;
+console.log(suma);
+// b)
+var firstWorld = "java";
+var secondWorld = "script";
+var concatenation = firstWorld + secondWorld;
+console.log(concatenation);
+// c)
+var firstWorld = "java";
+var secondWorld = "script";
+var concatenation = Number(firstWorld.length) + Number(secondWorld.length);
+console.log(concatenation);
+
+
+
